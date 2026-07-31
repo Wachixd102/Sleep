890f6df097afd8beda08609035de75bb6554e42b
Wachixd102 <alonenightcore02@gmail.com>
2026-07-31 11:57:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+import joblib
+import os
 from pathlib import Path
-
 
 # ML Libraries
 from sklearn.model_selection import train_test_split, GridSearchCV
